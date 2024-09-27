8f241a77c668208058765977eb8839f1670c72a7
Lucio Plancarte <lplancarte@cnm.edu>
2024-09-27 10:04:54 -0600

    Added varibles, functions, and constructors to Person Class

--- Person.java
+++ Person.java
@@ -1,10 +1,53 @@
 /**Programmer: Lucio Plancarte
-Created: 26 SEP 2024
+Created:  26 SEP 2024
+Modified: 27 SEP 2024 -Added meat/potatoes
 Description: Inheritance
 */
 
 class Person{
 
-//TODO
+	//Class Member Variables
+	private String name;
+	private int height; //cm
+	private int weight; //kg
+
+	//Constructors
+	public Person(String name, int height, int weight){
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public Person(){
+		name = "Waluigi";
+		height = 185;
+		weight = 90;
+	}
+
+	//Setters
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setHeight(int height){
+		this.height = height;
+	}
+
+	public void setWeight(int weight){
+		this.weight = weight;
+	}
+
+	//Getters
+	public String getName(){
+		return name;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
+	public int getWeight(){
+		return weight;
+	}
 
 }
